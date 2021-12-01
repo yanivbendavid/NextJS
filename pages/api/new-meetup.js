8232e5d944b632517b73371c3a7a24ea0cfd7f50
Yaniv Ben David <yanivbd@gmail.com>
2021-12-01 18:09:58 +0200

    Meetups from mongo atlas

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -2,8 +2,6 @@ import { MongoClient } from "mongodb";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    /*const {body: { title, image, address, description },} = req;*/
-
     try {
       const client = await MongoClient.connect(
         "mongodb+srv://yaniv:yaniv1981@reactdemoproject.sge8j.mongodb.net/meetups?retryWrites=true&w=majority"
@@ -11,7 +9,6 @@ export default async function handler(req, res) {
       const db = client.db();
       const meetupsCollection = db.collection("meetup");
       const result = await meetupsCollection.insertOne(req.body);
-      console.log(result);
       client.close();
       res.status(201).json({ message: "meetup inserted" });
     } catch (error) {
@@ -19,3 +16,24 @@ export default async function handler(req, res) {
     }
   }
 }
+
+/*
+export default function handler(req, res) {
+  return new Promise((resolve, reject) => {
+    if (req.method === "POST") {
+      try {
+        MongoClient.connect(
+          "mongodb+srv://yaniv:yaniv1981@reactdemoproject.sge8j.mongodb.net/meetups?retryWrites=true&w=majority"
+        )
+          .then((db) => db.collection("meetup"))
+          .then((result) => result.insertOne(req.body));
+        client.close();
+        resolve();
+      } catch (error) {
+        console.log(error);
+        reject("error connecting");
+      }
+    }
+  });
+}
+*/
